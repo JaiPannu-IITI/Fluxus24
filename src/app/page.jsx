4c4404bfbd57eee383ffb1bec8c15a29c84efeba
Jai Pannu <142983705+JaiPannu-IITI@users.noreply.github.com>
2024-11-09 04:25:52 +0530

    changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" w-full h-[100vh] flex items-center justify-center text-[12rem] ">
-    FLUXUS
-    </div>
+    <>
+    </>
   );
 }
