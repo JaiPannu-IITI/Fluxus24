d94291870e14832d7818a19151497dc85c6fc71c
MananJiwnani <mananjiwnani@gmail.com>
2024-11-11 12:12:41 +0530

    About us frame

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,10 @@
-import Image from "next/image";
+// import Image from "next/image";
+import AU_Carousel from "./components/AU_Frame";
 
 export default function Home() {
   return (
     <>
+      <AU_Carousel/>
     </>
   );
 }
