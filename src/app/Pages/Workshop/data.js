91408cd452761c894cf8a9c58cd7330243cf0dc7
mohitgarhewal <mohitgarhewal68@gmail.com>
2024-11-13 00:48:41 +0530

    Data updated in Workshop page

---
+++ src/app/Pages/Workshop/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        image: "https://via.placeholder.com/600x400",
+        title: "Competitive Programming (CP) / Data Structures & Algorithms (DSA)",
+        description: "This workshop focuses on enhancing your problem-solving skills through complex coding challenges and mastering efficient algorithms. You'll gain a deep understanding of data structures and algorithmic strategies, vital for building robust software solutions. Designed to boost your confidence, this hands-on training prepares you for coding competitions, technical interviews, and beyond.",
+    },
+    {
+        image: "https://via.placeholder.com/600x400",
+        title: "Artificial Intelligence (AI) & Machine Learning (ML)",
+        description: "Immerse yourself in the fascinating world of AI and ML, exploring everything from data preprocessing and model development to deployment. This workshop offers practical experience in building intelligent systems, complemented by real-world projects and case studies. Learn how AI-driven solutions are transforming industries and revolutionizing decision-making processes.",
+    },
+    {
+        image: "https://via.placeholder.com/600x400",
+        title: "Web Development",
+        description: "This workshop introduces you to the art of building dynamic, user-friendly websites and web applications using both front-end and back-end technologies. You'll explore essential frameworks, tools, and best practices needed to create modern, responsive web experiences. Perfect for aspiring developers, it offers the foundation and skills needed to excel in the fast-growing world of web technology.",
+    },
+    {
+        image: "https://via.placeholder.com/600x400",
+        title: "Marketing and Business",
+        description: "Gain a deep understanding of branding, market strategies, and consumer engagement in today’s competitive environment. This workshop delves into the nuances of successful marketing campaigns, market analysis, and business growth techniques. Ideal for budding marketers and entrepreneurs, it equips you with the skills to create impactful strategies and achieve business success.",
+    },
+];
