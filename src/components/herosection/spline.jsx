27db520e936ea5405a742a09ffb423005b8393be
Jai Pannu <142983705+JaiPannu-IITI@users.noreply.github.com>
2024-11-14 00:47:04 +0530

    changes

--- src/components/herosection/spline.jsx
+++ src/components/herosection/spline.jsx
@@ -1,18 +1,67 @@
 import Spline from '@splinetool/react-spline/next';
 
+import Link from 'next/link'
+
 export default function Home() {
+
+  const scrolls = [
+    {
+      name: 'Home',
+      link: '/'
+    },
+    {
+      name: 'About',
+      link: '/about'
+    },
+    {
+      name: 'Services',
+      link: '/services'
+    },
+    {
+      name: 'Contact',
+      link: '/contact'
+    },
+  ]
   return (
-    <main>
+    <div >
     
       <Spline
         scene="https://prod.spline.design/F3-FHsfqj4t-hIH7/scene.splinecode" 
         className= "  absolute w-[100dvw] h-[100dvh] "
       />
 
-<div className=' h-[70px] rounded-tl-3xl  w-[180px] bottom-[0px] right-0 absolute bg-[rgba(0,0,0,1)] '>
+      <div className=' absolute    h-[70px] bottom-[18px]  z-10 w-full flex justify-between px-[18px]' >
+
+<Link href="#">
+<div className=' flex items-center justify-center font-bold h-[70px] rounded-xl  w-[140px]   bg-[rgba(20,20,20,1)] hover:scale-[1.05] transition-all duration-300 '>
+      <img src="/logo/fluxus.png" alt="Fluxus 24" className=" h-[3rem] brightness-0 invert"/>
+</div>
+</Link>
+
+<Link href="#">
+<div className=' flex items-center justify-center font-bold h-[70px] rounded-xl  w-[140px]   bg-[rgba(20,20,20,1)] hover:scale-[1.05] transition-all duration-300 '>
+      <img src="/logo/fluxus.png" alt="Fluxus 24" className=" h-[3rem] brightness-0 invert"/>
+</div>
+</Link>
+<Link href="#">
+<div className=' flex items-center justify-center font-bold h-[70px] rounded-xl  w-[140px]   bg-[rgba(20,20,20,1)] hover:scale-[1.05] transition-all duration-300 '>
+      <img src="/logo/fluxus.png" alt="Fluxus 24" className=" h-[3rem] brightness-0 invert"/>
+</div>
+</Link>
+<Link href="#">
+<div className=' flex items-center justify-center font-bold h-[70px] rounded-xl  w-[140px]   bg-[rgba(20,20,20,1)] hover:scale-[1.05] transition-all duration-300 '>
+      <img src="/logo/fluxus.png" alt="Fluxus 24" className=" h-[3rem] brightness-0 invert"/>
+</div>
+</Link>
+<Link href="#">
+<div className=' flex items-center justify-center font-bold h-[70px] rounded-xl  w-[140px]   bg-[rgba(20,20,20,1)] hover:scale-[1.05] transition-all duration-300 '>
+      <img src="/logo/fluxus.png" alt="Fluxus 24" className=" h-[3rem] brightness-0 invert"/>
+</div>
+</Link>
+
 
 </div>
 
-    </main>
+    </div>
   );
 }
