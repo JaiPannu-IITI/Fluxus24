1ffc43590861d560d652aeddd1d8e8052c4b9a8f
Jai Pannu <142983705+JaiPannu-IITI@users.noreply.github.com>
2024-11-14 00:32:38 +0530

    changes major

--- src/components/herosection/spline.jsx
+++ src/components/herosection/spline.jsx
@@ -3,13 +3,10 @@ import Spline from '@splinetool/react-spline/next';
 export default function Home() {
   return (
     <main>
+    
       <Spline
-       scene="https://prod.spline.design/SwUc29cRI54eAPAJ/scene.splinecode" 
-                className= " absolute w-[100dvw] h-[100dvh]"
-      />
-      <Spline
-        scene="https://prod.spline.design/yRJ6gL146xvWBlxi/scene.splinecode" 
-        className= "  absolute w-[100dvw] h-[100dvh]"
+        scene="https://prod.spline.design/F3-FHsfqj4t-hIH7/scene.splinecode" 
+        className= "  absolute w-[100dvw] h-[100dvh] "
       />
 
 <div className=' h-[70px] rounded-tl-3xl  w-[180px] bottom-[0px] right-0 absolute bg-[rgba(0,0,0,1)] '>
