d000732ff6499483822c9aaaa62c49257ec194d4
Jai Pannu <142983705+JaiPannu-IITI@users.noreply.github.com>
2024-11-15 02:56:51 +0530

    changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,11 +7,11 @@ import Spline from "@/components/herosection/spline"
 export default function Home() {
   return (
     <>
-      {/* <Spline /> */}
+      <Spline />
     {/* <Navbar/>
-    <Workshop/> */}
-     {/* <AU_Carousel/> */}
-     <Workshop/>
+    <Workshop/>
+     <AU_Carousel/> */}
+     {/* <Workshop/> */}
     </>
   );
 }
--- src/components/herosection/spline.jsx
+++ src/components/herosection/spline.jsx
@@ -29,17 +29,16 @@ export default function Home() {
         scene="https://prod.spline.design/F3-FHsfqj4t-hIH7/scene.splinecode" 
         className= "  absolute w-[100dvw] h-[100dvh] "
       />
+
+      
      
 
       <div className=' absolute    h-[70px] bottom-[18px]  z-10 w-full flex justify-end px-[18px]' >
-
 <Link href="#">
 <div className=' flex items-center justify-center font-bold h-[70px] rounded-xl  w-[140px]   bg-[rgba(20,20,20,1)] hover:scale-[1.05] transition-all duration-300 '>
       <img src="/logo/fluxus.png" alt="Fluxus 24" className=" h-[3rem] brightness-0 invert"/>
 </div>
 </Link>
-
-
 </div>
 
     </div>
