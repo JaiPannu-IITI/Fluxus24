54f60730eb4a0e07a39d760dd525905c7d5e436e
Jai Pannu <142983705+JaiPannu-IITI@users.noreply.github.com>
2024-11-14 22:46:48 +0530

    chnages

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 // import Image from "next/image";
 import AU_Carousel from "../components/AU_Frame";
 import Navbar from "../components/Navbar";
-import Workshop from "./Pages/Workshop/Workshop";
+import Workshop from "./Pages/Workshop/page";
 import Spline from "@/components/herosection/spline"
 
 export default function Home() {
