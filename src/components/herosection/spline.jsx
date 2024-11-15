3f66cc50fa2cda84baa0d99683a777dcc584f56c
Jai Pannu <142983705+JaiPannu-IITI@users.noreply.github.com>
2024-11-15 11:17:34 +0530

    h

--- src/components/herosection/spline.jsx
+++ src/components/herosection/spline.jsx
@@ -1,46 +1,68 @@
-import Spline from '@splinetool/react-spline/next';
+import dynamic from "next/dynamic";
+import Link from "next/link";
 
-import Link from 'next/link'
+// Dynamically import the Spline component with lazy loading
+const Spline = dynamic(() => import("@splinetool/react-spline/next"), {
+  ssr: false, // Disable server-side rendering for Spline
+  loading: () => (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#000",
+        color: "#fff",
+        fontSize: "1rem",
+      }}
+    >
+      <img
+              src="/logo/fluxus.png"
+              alt="Fluxus 24"
+              className="h-[10rem] brightness-0 invert"
+            />
+    </div>
+  ),
+});
 
 export default function Home() {
-
   const scrolls = [
     {
-      name: 'Home',
-      link: '/'
+      name: "Home",
+      link: "/",
     },
     {
-      name: 'About',
-      link: '/about'
+      name: "About",
+      link: "/about",
     },
     {
-      name: 'Services',
-      link: '/services'
+      name: "Services",
+      link: "/services",
     },
     {
-      name: 'Contact',
-      link: '/contact'
+      name: "Contact",
+      link: "/contact",
     },
-  ]
+  ];
+
   return (
-    <div >
-    
+    <div>
       <Spline
-        scene="https://prod.spline.design/F3-FHsfqj4t-hIH7/scene.splinecode" 
-        className= "  absolute w-[100dvw] h-[100dvh] "
+        scene="https://prod.spline.design/F3-FHsfqj4t-hIH7/scene.splinecode"
+        className="absolute w-[100dvw] h-[100dvh]"
       />
 
-      
-     
-
-      <div className=' absolute    h-[70px] bottom-[18px]  z-10 w-full flex justify-end px-[18px]' >
-<Link href="#">
-<div className=' flex items-center justify-center font-bold h-[70px] rounded-xl  w-[140px]   bg-[rgba(20,20,20,1)] hover:scale-[1.05] transition-all duration-300 '>
-      <img src="/logo/fluxus.png" alt="Fluxus 24" className=" h-[3rem] brightness-0 invert"/>
-</div>
-</Link>
-</div>
-
+      <div className="absolute h-[70px] bottom-[18px] z-10 w-full flex justify-end px-[18px]">
+        <Link href="#">
+          <div className="flex items-center justify-center font-bold h-[70px] rounded-xl w-[140px] bg-[rgba(20,20,20,1)] hover:scale-[1.05] transition-all duration-300">
+            {/* <img
+              src="/logo/fluxus.png"
+              alt="Fluxus 24"
+              className="h-[3rem] brightness-0 invert"
+            /> */}
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
