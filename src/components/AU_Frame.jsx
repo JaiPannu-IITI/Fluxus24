f0206c90ea8beaedb16b7df95caaa9751991e05e
Jai Pannu <142983705+JaiPannu-IITI@users.noreply.github.com>
2024-11-15 13:11:39 +0530

    blob

--- src/components/AU_Frame.jsx
+++ src/components/AU_Frame.jsx
@@ -11,18 +11,18 @@ const OPTIONS = {
 const SLIDE_COUNT = 5
 // const SLIDES = ["/static/images/Image1.jpg", "/static/images/Image2.jpg", "/static/images/Image3.jpg", "/static/images/Image4.jpg", "/static/images/Image5.jpg"]
 const SLIDES = [
-    "/static/images/carouselImages/img1.jpg",
-    "/static/images/carouselImages/img2.jpg",
-    "/static/images/carouselImages/img3.jpg",
-    "/static/images/carouselImages/img4.jpg",
-    "/static/images/carouselImages/img5.jpg",
-    "/static/images/carouselImages/img6.jpg",
-    "/static/images/carouselImages/img7.jpg",
-    "/static/images/carouselImages/img8.jpg",
-    "/static/images/carouselImages/img9.jpg",
-    "/static/images/carouselImages/img10.jpg",
-    "/static/images/carouselImages/img11.jpg",
-    "/static/images/carouselImages/img12.jpg"
+    "https://vhk1meghamdhsbee.public.blob.vercel-storage.com/carouselImages/img1-9Yt8vuZIrLUC07mDlY66XyTdLB0THj.jpg",
+    "https://vhk1meghamdhsbee.public.blob.vercel-storage.com/carouselImages/img2-XSj4kgYU21wRtkCPMtYgBOuysCBv2a.jpg",
+    "https://vhk1meghamdhsbee.public.blob.vercel-storage.com/carouselImages/img3-mzliEmie5GJLvbfzuPMZe4v9mePaXk.jpg",
+    "https://vhk1meghamdhsbee.public.blob.vercel-storage.com/carouselImages/img4-UELywVxWGJtPl96npp7WGDVY41HpVZ.jpg",
+    "https://vhk1meghamdhsbee.public.blob.vercel-storage.com/carouselImages/img5-XTElf8tCwtBb29MRFwn9spvi2ljD2F.jpg",
+    "https://vhk1meghamdhsbee.public.blob.vercel-storage.com/carouselImages/img6-LdG9jWphV14al5RtzQjHK8QXm3UxGd.jpg",
+    "https://vhk1meghamdhsbee.public.blob.vercel-storage.com/carouselImages/img7-1Ph8mW9LArSHLFFdTDmMLdKlLZI34M.jpg",
+    "https://vhk1meghamdhsbee.public.blob.vercel-storage.com/carouselImages/img8-nSwu7qYbbKcMil89L0y6sfQQlVZwDC.jpg",
+    "https://vhk1meghamdhsbee.public.blob.vercel-storage.com/carouselImages/img9-RHGqJ7W6ixq8UNggewBehIRuttLyyl.jpg",
+    "https://vhk1meghamdhsbee.public.blob.vercel-storage.com/carouselImages/img10-NHYA7LGBLCRJ1xqKXfPMHVcA5mNGY4.jpg",
+    "https://vhk1meghamdhsbee.public.blob.vercel-storage.com/carouselImages/img11-Ee5CS0UF9v1KUNcLfZUCUb8ehYzJMW.jpg",
+    "https://vhk1meghamdhsbee.public.blob.vercel-storage.com/carouselImages/img12-pvMREnp4RZ5F4FqNjaTjPqkfWjta2Y.jpg"
   ];
 
 const AU_Frame = () => {
