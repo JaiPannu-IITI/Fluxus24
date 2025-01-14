7aa6af539659722f9ce1c6323536c6a755e9f207
Jai Pannu <142983705+JaiPannu-IITI@users.noreply.github.com>
2025-01-14 21:57:49 +0530

    changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,13 +4,12 @@ import Spline from "@/components/herosection/spline"
 import Ca_responsibilities from "@/components/ca_responsibilities";
 import CampusAmbassadorIncentives from "../components/Incentives&whyCA/caincentives";
 import WhyCA from "@/components/Incentives&whyCA/WhyCA";
-
+import Loader from "@/components/ca/loading"
 export default function Home() {
   return (
     <>
       <Spline />
-      <WhyCA />
-      <CampusAmbassadorIncentives />
+    <Loader/>
     </>
   );
 }
