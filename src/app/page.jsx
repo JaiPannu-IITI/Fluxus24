210b9081f8ba07e41332bc18ff5694591edfd07e
mohitgarhewal <mohitgarhewal68@gmail.com>
2025-02-02 19:50:52 +0530

    accomodation page added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,13 @@
+import Accomodation from "@/components/accomodation/Accomodation";
+import PhotoGallery from "@/components/accomodation/Photos";
 import Herosection from "@/components/herosection/main"
+
 export default function Home() {
   return (
     <>
       <Herosection />
-
+      {/* <Accomodation/>
+      <PhotoGallery/> */}
     </>
   );
 }
