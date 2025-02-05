39e39ab740b4d7f0ae8f8203932ba2398dbcd54f
Sameer Choudhary <142528209+samnotfound404@users.noreply.github.com>
2025-02-05 22:47:00 +0530

    overrite

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,15 @@
-import Accomodation from "@/components/accomodation/Accomodation";
-import PhotoGallery from "@/components/accomodation/Photos";
+'use client';
+import Accomodation from "@/app/accomodation/page";
+import PhotoGallery from "@/app/accomodation/Photos";
 import Herosection from "@/components/herosection/main"
-
+import Demo from "@/components/landingpage/main"
 export default function Home() {
   return (
     <>
       <Herosection />
       {/* <Accomodation/>
       <PhotoGallery/> */}
+      <Demo/>
     </>
   );
 }
