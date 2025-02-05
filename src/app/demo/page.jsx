fa223d1a295b5b2abbf6f53ede51b601ec863e7c
Sameer Choudhary <142528209+samnotfound404@users.noreply.github.com>
2025-02-05 22:20:29 +0530

    Revert "horizontal scroll"
    This reverts commit a5677a4a5ee5450ccd134347bd15f3647ea08908.

--- src/app/demo/page.jsx
+++
@@ -1,7 +0,0 @@
-'use client'
-import ParallaxSection from "@/components/landingPage/parallax-section";
-import App from "@/components/landingPage/app";
-
-export default function Page() {
-    return <App/>;
-  }
