f31b9e3835b5d970d2b08a8bb62368b6bb6d16b6
mohitgarhewal <mohitgarhewal68@gmail.com>
2025-02-05 18:44:42 +0530

    change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,8 @@ import Herosection from "@/components/herosection/main"
 export default function Home() {
   return (
     <>
-      {/* <Herosection /> */}
-      <Accomodation/>
+      <Herosection />
+      {/* <Accomodation/> */}
       
     </>
   );
