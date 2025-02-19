55a03497cb1b51b89305e8adf8e2ec838642b43a
mohitgarhewal <mohitgarhewal68@gmail.com>
2025-02-19 18:50:25 +0530

    gallery page updated

--- src/app/accomodation/page.jsx
+++ src/app/accomodation/page.jsx
@@ -84,8 +84,8 @@ const Accomodation = () => {
       phone: "+91 81081 06458",
       image: "/images/Accomodation/shre.jpg", // Add correct image path
       social: {
-        linkedin: "#",
-        instagram: "#",
+        linkedin: "https://www.linkedin.com/in/shreyas-kale-5a3271280",
+        instagram: "https://www.instagram.com/its_shrey04/",
        
       },
     },
@@ -237,34 +237,34 @@ const Accomodation = () => {
       </div>
 
       {/*  Contact Card Section */}
-      <div className="container mx-auto px-4 py-8 mb-10 ">
-        <h2 className={`${pressStart2P.className} text-2xl md:text-3xl text-white mt-12 mb-6`}>Contact:</h2>
-        <div className="flex flex-wrap justify-center gap-8">
-          {contacts.map((contact, index) => (
-            <div key={index} className="bg-gray-900 w-72 h-64 rounded-lg shadow-lg flex flex-col items-center p-4">
-              <Image
-                src={contact.image}
-                alt={contact.name}
-                width={100}
-                height={100}
-                className="w-24 h-24 rounded-full object-cover border-2 border-white"
-              />
-              <h3 className="text-xl font-semibold mt-3">{contact.name}</h3>
-              <p className="text-sm text-gray-400">{contact.designation}</p>
-              <p className="text-md font-medium mt-2">{contact.phone}</p>
-              <div className="flex space-x-4 mt-3">
-                <a href={contact.social.linkedin} target="_blank" rel="noopener noreferrer">
-                  <FaLinkedin className="text-blue-400 text-xl hover:text-blue-600 transition" />
-                </a>
-                <a href={contact.social.instagram} target="_blank" rel="noopener noreferrer">
-                  <FaInstagram className="text-pink-400 text-xl hover:text-pink-600 transition" />
-                </a>
-               
-              </div>
-            </div>
-          ))}
+      <div className="container mx-auto px-4 py-8 mb-10">
+  <h2 className={`${pressStart2P.className} text-2xl md:text-3xl text-white mt-12 mb-6`}>Contact:</h2>
+  <div className="flex flex-wrap justify-center gap-8">
+    {contacts.map((contact, index) => (
+      <div key={index} className="bg-gray-900 w-72 h-64 rounded-lg shadow-lg flex flex-col items-center p-4">
+        <Image
+          src={contact.image}
+          alt={contact.name}
+          width={100}
+          height={100}
+          className="w-24 h-24 rounded-full object-cover border-2 border-white"
+        />
+        <h3 className="text-xl font-semibold mt-3">{contact.name}</h3>
+        <p className="text-sm text-gray-400">{contact.designation}</p>
+        <p className="text-md font-medium mt-2">{contact.phone}</p>
+        <div className="flex space-x-4 mt-3">
+          <a href={`${contact.social.linkedin}`} target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="text-blue-400 text-xl hover:text-blue-600 transition" />
+          </a>
+          <a href={`${contact.social.instagram}`} target="_blank" rel="noopener noreferrer">
+            <FaInstagram className="text-pink-400 text-xl hover:text-pink-600 transition" />
+          </a>
         </div>
       </div>
+    ))}
+  </div>
+</div>
+
       {/* Modal Div */}
       {isModalVisible && (
         <div className="fixed inset-0 flex items-start justify-center bg-black bg-opacity-60  backdrop-blur-sm z-50 pt-10 mt-10 ">
--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -4,7 +4,7 @@ import { Press_Start_2P } from "next/font/google"
 import { Pixelify_Sans } from "next/font/google"
 import { motion } from "framer-motion"
 import { useState, useEffect, useRef } from "react"
-import Image from "next/image"; 
+import Image from "next/image"
 
 const images = [
   "/images/carouselImages/img1.jpg",
@@ -36,6 +36,7 @@ const Gallery = () => {
   const [flippedIndex, setFlippedIndex] = useState(null)
   const [scrollY, setScrollY] = useState(0)
   const backgroundRef = useRef(null)
+  const bgref = useRef(null)
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -56,8 +57,15 @@ const Gallery = () => {
 
   useEffect(() => {
     if (backgroundRef.current) {
-      const scale = 1 + scrollY * 0.00015 
+      const scale = 1 + scrollY * 0.00015
       backgroundRef.current.style.transform = `scale(${scale})`
+    }
+  }, [scrollY])
+
+  useEffect(() => {
+    if (bgref.current) {
+      const scale = 1 + scrollY * 0.003
+      bgref.current.style.transform = `scale(${scale})`
     }
   }, [scrollY])
 
@@ -66,9 +74,20 @@ const Gallery = () => {
       {/* Fixed Background with Parallax Zoom Effect */}
       <div
         ref={backgroundRef}
-        className="fixed top-0 left-0 w-full h-full z-[-1] brightness-75 transition-transform duration-300 ease-out"
+        className="fixed top-0 left-0 w-full h-full z-[-2] brightness-75 transition-transform duration-300 ease-out"
         style={{
           backgroundImage: `url(/gallery/bggallery.jpg)`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundAttachment: "fixed",
+        }}
+      ></div>
+      <div
+        ref={bgref}
+        className="fixed top-0 left-0 w-full h-full z-[-1] brightness-75 transition-transform duration-300 ease-out"
+        style={{
+          backgroundImage: `url(/gallery/bgp.png)`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
@@ -78,32 +97,30 @@ const Gallery = () => {
 
       {/* Scrollable Content */}
       <div className="relative w-full min-h-screen overflow-y-auto">
-        
-        <h1 className={`${pressStart2P.className} text-5xl md:text-6xl text-center text-zinc-300 mt-20 p-5  bg-black bg-opacity-50 backdrop-blur-lg rounded-md mx-auto w-3/4`}>
+        <h1
+          className={`${pressStart2P.className} text-5xl md:text-6xl text-center text-zinc-300 mt-32 p-5 mb `}
+        >
           Gallery
         </h1>
 
         <div className="text-gray-600 body-font">
-          <div className="container px-5 py-20 mx-auto flex flex-wrap">
+          <div className="container px-2 sm:px-5 pt-18 md:pt-20  pb-24 mx-auto">
             {[...Array(3)].map((_, sectionIndex) => (
               <div key={sectionIndex} className="flex flex-wrap w-full md:-m-2 -m-1 mt-10 pt-2">
-                <div className="flex flex-wrap w-1/2">
-
-
+                <div className="flex flex-wrap w-full md:w-1/2">
                   {/* small images on the left */}
-                  {[0, 1, 2].map((i) => ( 
-                    <div
-                      key={i}
-                      className={`md:p-2 p-1 w-1/2 mt-3 pt-2 ${i === 2 ? "mx-auto" : ""}`}
-                    >
+                  {[0, 1, 2].map((i) => (
+                    <div key={i} className={`md:p-2 p-1 w-1/2 mt-3 pt-2 ${i === 2 ? "mx-auto" : ""}`}>
                       <motion.div
                         className="relative"
                         animate={{ rotateY: flippedIndex === sectionIndex * 6 + i ? 180 : 0 }}
                         transition={{ duration: 0.6 }}
                       >
-                        <div className="w-full h-full object-cover object-center block bg-white text-white font-medium border border-black pr-1 pb-1 rounded-md 
+                        <div
+                          className="w-full h-full object-cover object-center block bg-white text-white font-medium border border-black pr-1 pb-1 rounded-md 
              shadow-[6px_6px_0px_0px_black] transition-transform duration-200 
-             hover:scale-105 hover:shadow-[8px_8px_0px_0px_black]">
+             hover:scale-105 hover:shadow-[8px_8px_0px_0px_black]"
+                        >
                           <Image
                             height={300}
                             width={520}
@@ -116,98 +133,73 @@ const Gallery = () => {
                     </div>
                   ))}
 
-
-                   {/* big images */}
+                  {/* big image */}
                   <div className="md:p-2 p-1 w-full mt-2">
                     <motion.div
                       className="relative"
                       animate={{ rotateY: flippedIndex === sectionIndex * 6 + 3 ? 180 : 0 }}
                       transition={{ duration: 0.6 }}
                     >
-                      <div className="w-full h-full object-cover object-center block bg-white text-white font-medium border border-black pr-1 pb-1  rounded-md 
-                                     shadow-[6px_6px_0px_0px_black] transition-transform duration-200 
-                                  hover:scale-105 hover:shadow-[8px_8px_0px_0px_black]">
+                      <div
+                        className="w-full aspect-[16/9] object-cover object-center block bg-white text-white font-medium border border-black pr-1 pb-1 rounded-md 
+             shadow-[6px_6px_0px_0px_white] transition-transform duration-200 
+             hover:scale-105 hover:shadow-[8px_8px_0px_0px_white]"
+                      >
                         <Image
-                          height={600}
-                          width={1050}
+                          fill
                           alt="gallery"
-                          className="w-full h-full object-cover object-center block"
+                          className="object-cover object-center"
                           src={images[3] || "/placeholder.svg"}
                         />
-
                       </div>
-
                     </motion.div>
                   </div>
                 </div>
-                <div className="flex flex-wrap w-1/2 mt-2 ">
-                  <div className="md:p-2 p-1 w-full ml-4">
+                <div className="flex flex-wrap w-full md:w-1/2 mt-2">
+                  <div className="md:p-2 p-1 w-full">
                     <motion.div
                       className="relative"
                       animate={{ rotateY: flippedIndex === sectionIndex * 6 + 4 ? 180 : 0 }}
                       transition={{ duration: 0.6 }}
                     >
-                      <div className="h-full w-[525px]  object-cover object-center block bg-white text-white font-medium border border-black pr-1 pb-1  rounded-md 
+                      <div
+                        className="w-full aspect-[16/9] object-cover object-center block bg-white text-white font-medium border border-black pr-1 pb-1 rounded-md 
                                      shadow-[6px_6px_0px_0px_black] transition-transform duration-200 
-                                     hover:scale-105 hover:shadow-[8px_8px_0px_0px_black] ">
+                                     hover:scale-105 hover:shadow-[8px_8px_0px_0px_black]"
+                      >
                         <Image
-                          height={300}
-                          width={520}
+                          fill
                           alt="gallery"
-                          className="w-[520px] h-[300px] object-cover object-center block"
+                          className="object-cover object-center"
                           src={images[4] || "/placeholder.svg"}
                         />
-
                       </div>
-
                     </motion.div>
                   </div>
 
-                  {[0, 1, 2].map((i) => ( 
-                    <div
-                      key={i}
-                      className={`md:p-2 p-1 w-1/2 mt-3 pt-2 ${i === 2 ? "mx-auto" : ""}`}
-                    >
+                  {[0, 1, 2].map((i) => (
+                    <div key={i} className={`md:p-2 p-1 w-1/2 mt-3 pt-2 ${i === 2 ? "mx-auto" : ""}`}>
                       <motion.div
                         className="relative"
-                        animate={{ rotateY: flippedIndex === sectionIndex * 6 + i ? 180 : 0 }}
+                        animate={{ rotateY: flippedIndex === sectionIndex * 6 + i + 5 ? 180 : 0 }}
                         transition={{ duration: 0.6 }}
                       >
-                        <div className="w-full h-full object-cover object-center block bg-white text-white font-medium border border-black pr-1 pb-1 rounded-md 
+                        <div
+                          className="w-full h-full object-cover object-center block bg-white text-white font-medium border border-black pr-1 pb-1 rounded-md 
              shadow-[6px_6px_0px_0px_black] transition-transform duration-200 
-             hover:scale-105 hover:shadow-[8px_8px_0px_0px_black]">
+             hover:scale-105 hover:shadow-[8px_8px_0px_0px_black]"
+                        >
                           <Image
                             height={300}
                             width={520}
                             alt="gallery"
                             className="w-full object-cover h-full object-center block"
-                            src={images[i] || "/placeholder.svg"}
+                            src={images[i + 5] || "/placeholder.svg"}
                           />
                         </div>
                       </motion.div>
                     </div>
                   ))}
-
-
-                  {/* {[5].map((i) => ( 
-                    <div key={i} className="md:p-2 p-1 w-1/2 mt-2 ml-5">
-                      <motion.div
-                        className="relative"
-                        animate={{ rotateY: flippedIndex === sectionIndex * 6 + i ? 180 : 0 }}
-                        transition={{ duration: 0.6 }}
-                      >
-                        <div className="w-full h-full object-cover object-center block bg-white text-white font-medium border border-black pr-1 pb-1  rounded-md 
-                                       shadow-[6px_6px_0px_0px_black] transition-transform duration-200 
-                                       hover:scale-105 hover:shadow-[8px_8px_0px_0px_black]">
-                          <img
-                            alt="gallery"
-                            className="w-full object-cover h-full object-center block"
-                            src={images[i] || "/placeholder.svg"}
-                          />
-                        </div>
-                      </motion.div>
-                    </div>
-                  ))} */}
                 </div>
               </div>
             ))}
