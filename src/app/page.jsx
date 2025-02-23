4449a83c0451dbab2c51521298f76edf3810251e
Sameer Choudhary <142528209+samnotfound404@users.noreply.github.com>
2025-02-23 21:11:47 +0530

    glitchy title

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,8 @@ import PhotoGallery from "@/app/accomodation/Photos";
 import Herosection from "@/components/herosection/main"
 import Demo from "@/components/landingpage/main"
 import Team from "@/components/ui/Team";
+import GlitchyTitle from "@/components/main/title";
+import Landing from "@/components/main/landing";
 export default function Home() {
   return (
     <>
@@ -11,6 +13,7 @@ export default function Home() {
       {/* <Accomodation/>
       <PhotoGallery/> */}
       {/* <Demo/> */}
+      {/* <Landing/> */}
       <Team/>
     </>
   );
